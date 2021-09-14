77db1e63d6118a73caddd55398d3678977b4aa3e
BRUNO RIBEIRO DE OLIVEIRA <21193381@CFP127ALUNO.LOCAL>
2021-09-14 16:05:18 -0300

    projeto atualizado

--- lampada.js
+++ lampada.js
@@ -1,9 +1,11 @@
 "use strict"
 
 const lampada = document.getElementById("lampada")
+let idInterval
+
 
 function lampadaInteira(){
-    return !lampada.src.includes ("quebrada")
+    return lampada.src.includes ("ligada")
 //  return lampada.src.includes ("ligada")
     /* includes ("ligada") = verifica sobre a variavel lampada 
     no nome do arquivo "src" se existe a palavra "ligada" e se existir, o valor é true e se nao existir é false */
@@ -26,8 +28,6 @@ function quebrarLampada(){
     lampada.src = "img/quebrada.jpg"
 }
 
-
-
 //setInterval = (funcao, intervalo(milisegundos)) nao para nunca
 //setTimeout = (funcao, intervalo) - para de acordo com o limite do while
 //clearInternal = (idInterval) = faz parar o setInterval ou o setTimeout
@@ -48,6 +48,34 @@ function piscarSetInterval(){
   setInterval(desligarLampada, 1000)
 }
 
+function lampadaDesligada(){
+    return lampada.src.includes ("desligada")
+}
+
+function trocarImagem(){
+    if(lampadaDesligada()){
+        ligarLampada()
+    }
+    else{
+        desligarLampada()
+    }
+}
+
+function pararPiscar(){
+    clearInterval(idInterval)
+}
+function piscarLampada() {
+
+    const piscar = document.getElementById("piscar")
+    if (piscar.textContent == "Piscar") {
+        idInterval = setInterval(trocarImagem, 1000)
+        piscar.textContent = "Parar"
+    } else {
+        pararPiscar()
+        piscar.textContent = "Piscar"
+    }
+    
+}
 
 
 //eventos                              
@@ -58,8 +86,7 @@ document.getElementById("desligar")
     .addEventListener("click", desligarLampada)
 
 document.getElementById("piscar")
-    .addEventListener("click", piscarSetInterval)
-
+    .addEventListener("click", piscarLampada)
 
 
 //Passar mouse sobre a lampada e alterar o evento
@@ -68,8 +95,5 @@ lampada.addEventListener("mouseover", ligarLampada)
 //mouseover = quando passar o mouse sobre a imagem
 lampada.addEventListener("mouseout", desligarLampada)
 //mouseout = quando tirar o mouse sobre a imagem
-
-
-document.getElementById("lampada")
-    .addEventListener("dblclick", quebrarLampada)
+lampada.addEventListener("dblclick", quebrarLampada)
 //dblclick = quando der dois cliques sobre algum objeto.
